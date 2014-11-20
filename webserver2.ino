70f17eb7a9bd3c822b16b31a924145db500942c8
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2014-11-19 20:54:38 -0600

    Se añade capacidad de leer tarjetas SD

--- webserver2.ino
+++ webserver2.ino
@@ -1,21 +1,38 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
 
 
 byte mac[] = { 0x71, 0x03, 0x58, 0x9B, 0xB5, 0xE7}; // MAC Address del dispositivo.
-
 EthernetServer server(80); // Puerto de escucha de el servidor Web.
+const int sdchipSelect = 4;
+File indexFile;
 
 void setup(){
     
     Serial.begin(9600);
     
+    Serial.println("Iniciando el Servidor...");
     Ethernet.begin(mac); // Inicia la red DHCP
     server.begin();      // Inicia el Servidor
     
     // Debug: Para ver que IP fue asignado al dispositivo.
     Serial.print("El servidor esta corriendo en: ");
     Serial.println(Ethernet.localIP());
+    
+    
+    Serial.println("Iniciando la tarjeta SD...");
+    if(!SD.begin(sdchipSelect)){
+        
+        Serial.println("Fallo al iniciar la tarjeta SD");
+        Serial.println("Cancelando secuencia de inicio...");
+        return;
+        
+    } else{
+        
+        Serial.println("Tarjeta SD inicializada...");
+        
+    }
   
 }
 
@@ -57,7 +74,21 @@ void loop(){
                     client.println("Content-Type: text/html");
                     client.println("Connection: close");
                     client.println();
-                    client.println("<h1>Hola Mundo!</h1>");
+                    
+                    indexFile = SD.open("index.htm");
+                    if(indexFile){
+                        
+                        while(indexFile.available()){
+                            client.write(indexFile.read());
+                        }
+                        
+                        indexFile.close();
+                        
+                    } else{
+                        
+                        client.print("Error!!");
+                        
+                    }
                     
                     break; // Para salir del loop cuando se termine de enviar la respuesta
                     
